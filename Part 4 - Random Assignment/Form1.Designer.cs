7a77a2858d4c7f6ed2065b1d2b9d49ad838ee967
Carter Chapman <ChapCart405@GoTVDSB.ca>
2022-02-25 10:14:58 -0500

    Add project files.

---
+++ Part 4 - Random Assignment/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Part_4___Random_Assignment
+{
+    partial class RandomNumbers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.lblMinimum = new System.Windows.Forms.Label();
+            this.lblMaximum = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnInteger = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Location = new System.Drawing.Point(12, 9);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(180, 13);
+            this.lblInstruction.TabIndex = 0;
+            this.lblInstruction.Text = "Enter a minimum and maximum value";
+            // 
+            // lblMinimum
+            // 
+            this.lblMinimum.AutoSize = true;
+            this.lblMinimum.Location = new System.Drawing.Point(12, 31);
+            this.lblMinimum.Name = "lblMinimum";
+            this.lblMinimum.Size = new System.Drawing.Size(48, 13);
+            this.lblMinimum.TabIndex = 1;
+            this.lblMinimum.Text = "Minimum";
+            // 
+            // lblMaximum
+            // 
+            this.lblMaximum.AutoSize = true;
+            this.lblMaximum.Location = new System.Drawing.Point(12, 54);
+            this.lblMaximum.Name = "lblMaximum";
+            this.lblMaximum.Size = new System.Drawing.Size(51, 13);
+            this.lblMaximum.TabIndex = 2;
+            this.lblMaximum.Text = "Maximum";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(66, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnInteger
+            // 
+            this.btnInteger.Location = new System.Drawing.Point(24, 77);
+            this.btnInteger.Name = "btnInteger";
+            this.btnInteger.Size = new System.Drawing.Size(75, 23);
+            this.btnInteger.TabIndex = 5;
+            this.btnInteger.Text = "Get Integer";
+            this.btnInteger.UseVisualStyleBackColor = true;
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.Location = new System.Drawing.Point(117, 77);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(75, 23);
+            this.btnDouble.TabIndex = 6;
+            this.btnDouble.Text = "Get Double";
+            this.btnDouble.UseVisualStyleBackColor = true;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(90, 103);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(39, 13);
+            this.lblOutput.TabIndex = 8;
+            this.lblOutput.Text = "Output";
+            this.lblOutput.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // RandomNumbers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 219);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.btnInteger);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblMaximum);
+            this.Controls.Add(this.lblMinimum);
+            this.Controls.Add(this.lblInstruction);
+            this.Name = "RandomNumbers";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Label lblMinimum;
+        private System.Windows.Forms.Label lblMaximum;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnInteger;
+        private System.Windows.Forms.Button btnDouble;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
