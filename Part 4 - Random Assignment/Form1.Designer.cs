b168c6b807d42916189943a00e1de2f6ea1262e2
Carter Chapman <ChapCart405@GoTVDSB.ca>
2022-03-01 09:45:02 -0500

    Finished

--- Part 4 - Random Assignment/Form1.Designer.cs
+++ Part 4 - Random Assignment/Form1.Designer.cs
@@ -31,17 +31,19 @@
             this.lblInstruction = new System.Windows.Forms.Label();
             this.lblMinimum = new System.Windows.Forms.Label();
             this.lblMaximum = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
             this.btnInteger = new System.Windows.Forms.Button();
             this.btnDouble = new System.Windows.Forms.Button();
             this.lblOutput = new System.Windows.Forms.Label();
+            this.lblDecimalPlaces = new System.Windows.Forms.Label();
+            this.txtDecimalPlaces = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblInstruction
             // 
             this.lblInstruction.AutoSize = true;
-            this.lblInstruction.Location = new System.Drawing.Point(12, 9);
+            this.lblInstruction.Location = new System.Drawing.Point(15, 9);
             this.lblInstruction.Name = "lblInstruction";
             this.lblInstruction.Size = new System.Drawing.Size(180, 13);
             this.lblInstruction.TabIndex = 0;
@@ -50,7 +52,7 @@
             // lblMinimum
             // 
             this.lblMinimum.AutoSize = true;
-            this.lblMinimum.Location = new System.Drawing.Point(12, 31);
+            this.lblMinimum.Location = new System.Drawing.Point(15, 33);
             this.lblMinimum.Name = "lblMinimum";
             this.lblMinimum.Size = new System.Drawing.Size(48, 13);
             this.lblMinimum.TabIndex = 1;
@@ -59,70 +61,90 @@
             // lblMaximum
             // 
             this.lblMaximum.AutoSize = true;
-            this.lblMaximum.Location = new System.Drawing.Point(12, 54);
+            this.lblMaximum.Location = new System.Drawing.Point(15, 66);
             this.lblMaximum.Name = "lblMaximum";
             this.lblMaximum.Size = new System.Drawing.Size(51, 13);
             this.lblMaximum.TabIndex = 2;
             this.lblMaximum.Text = "Maximum";
             // 
-            // textBox1
+            // txtMin
             // 
-            this.textBox1.Location = new System.Drawing.Point(66, 28);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(147, 20);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtMin.Location = new System.Drawing.Point(86, 30);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(147, 20);
+            this.txtMin.TabIndex = 3;
+            this.txtMin.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // txtMax
             // 
-            this.textBox2.Location = new System.Drawing.Point(66, 51);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(147, 20);
-            this.textBox2.TabIndex = 4;
+            this.txtMax.Location = new System.Drawing.Point(86, 63);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(147, 20);
+            this.txtMax.TabIndex = 4;
+            this.txtMax.TextChanged += new System.EventHandler(this.txtMax_TextChanged);
             // 
             // btnInteger
             // 
-            this.btnInteger.Location = new System.Drawing.Point(24, 77);
+            this.btnInteger.Location = new System.Drawing.Point(39, 122);
             this.btnInteger.Name = "btnInteger";
             this.btnInteger.Size = new System.Drawing.Size(75, 23);
             this.btnInteger.TabIndex = 5;
             this.btnInteger.Text = "Get Integer";
             this.btnInteger.UseVisualStyleBackColor = true;
+            this.btnInteger.Click += new System.EventHandler(this.btnInteger_Click);
             // 
             // btnDouble
             // 
-            this.btnDouble.Location = new System.Drawing.Point(117, 77);
+            this.btnDouble.Location = new System.Drawing.Point(138, 122);
             this.btnDouble.Name = "btnDouble";
             this.btnDouble.Size = new System.Drawing.Size(75, 23);
             this.btnDouble.TabIndex = 6;
             this.btnDouble.Text = "Get Double";
             this.btnDouble.UseVisualStyleBackColor = true;
+            this.btnDouble.Click += new System.EventHandler(this.btnDouble_Click);
             // 
             // lblOutput
             // 
-            this.lblOutput.AutoSize = true;
-            this.lblOutput.Location = new System.Drawing.Point(90, 103);
+            this.lblOutput.Location = new System.Drawing.Point(12, 148);
             this.lblOutput.Name = "lblOutput";
-            this.lblOutput.Size = new System.Drawing.Size(39, 13);
-            this.lblOutput.TabIndex = 8;
+            this.lblOutput.Size = new System.Drawing.Size(230, 23);
+            this.lblOutput.TabIndex = 7;
             this.lblOutput.Text = "Output";
-            this.lblOutput.Click += new System.EventHandler(this.label2_Click);
+            this.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDecimalPlaces
+            // 
+            this.lblDecimalPlaces.AutoSize = true;
+            this.lblDecimalPlaces.Location = new System.Drawing.Point(0, 99);
+            this.lblDecimalPlaces.Name = "lblDecimalPlaces";
+            this.lblDecimalPlaces.Size = new System.Drawing.Size(80, 13);
+            this.lblDecimalPlaces.TabIndex = 8;
+            this.lblDecimalPlaces.Text = "Decimal Places";
+            // 
+            // txtDecimalPlaces
+            // 
+            this.txtDecimalPlaces.Location = new System.Drawing.Point(86, 96);
+            this.txtDecimalPlaces.Name = "txtDecimalPlaces";
+            this.txtDecimalPlaces.Size = new System.Drawing.Size(147, 20);
+            this.txtDecimalPlaces.TabIndex = 9;
             // 
             // RandomNumbers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(353, 219);
+            this.ClientSize = new System.Drawing.Size(265, 176);
+            this.Controls.Add(this.txtDecimalPlaces);
+            this.Controls.Add(this.lblDecimalPlaces);
             this.Controls.Add(this.lblOutput);
             this.Controls.Add(this.btnDouble);
             this.Controls.Add(this.btnInteger);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
             this.Controls.Add(this.lblMaximum);
             this.Controls.Add(this.lblMinimum);
             this.Controls.Add(this.lblInstruction);
             this.Name = "RandomNumbers";
-            this.Text = "Form1";
+            this.Text = "Random Numbers";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,11 +155,13 @@
         private System.Windows.Forms.Label lblInstruction;
         private System.Windows.Forms.Label lblMinimum;
         private System.Windows.Forms.Label lblMaximum;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
         private System.Windows.Forms.Button btnInteger;
         private System.Windows.Forms.Button btnDouble;
         private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblDecimalPlaces;
+        private System.Windows.Forms.TextBox txtDecimalPlaces;
     }
 }
 
--- Part 4 - Random Assignment/Form1.cs
+++ Part 4 - Random Assignment/Form1.cs
@@ -12,17 +12,58 @@ namespace Part_4___Random_Assignment
 {
     public partial class RandomNumbers : Form
     {
+        Random generator = new Random();
+        int intOutput;
+        int Minimum;
+        int Maximum;
+        double randomDecimal;
+        double doubleOutput;
+        int decimalPlaces;
         public RandomNumbers()
+
         {
             InitializeComponent();
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInteger_Click(object sender, EventArgs e)
+        {
+            Minimum = Convert.ToInt32(txtMin.Text);
+            Maximum = Convert.ToInt32(txtMax.Text);
+            Maximum = Maximum + 1;
+            intOutput = generator.Next(Minimum, Maximum);
+            lblOutput.Text = ($"{intOutput}");
+        }
+        private void txtOutput_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnDouble_Click(object sender, EventArgs e)
+        {
+            Minimum = Convert.ToInt32(txtMin.Text);
+            Maximum = Convert.ToInt32(txtMax.Text);
+            decimalPlaces = Convert.ToInt32(txtDecimalPlaces.Text);
+            Maximum = Maximum - 1;
+            intOutput = generator.Next(Minimum, Maximum);
+            randomDecimal = generator.NextDouble();
+            doubleOutput = intOutput + randomDecimal;
+            doubleOutput = Math.Round(doubleOutput, decimalPlaces);
+            lblOutput.Text = ($"{doubleOutput}");
+
+        }
+
+        private void txtMax_TextChanged(object sender, EventArgs e)
         {
 
         }
